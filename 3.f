d10b41950f7f2372aa043f90f9a78ef9fe5ba0b0
stdmedoth <jovictor210@gmail.com>
2024-08-26 20:11:48 -0300

    finished 3.f

--- 3.f
+++ 3.f
@@ -11,14 +11,14 @@ C O resultado da ordenação, juntamente com o número M, é salvo em um arquivo
 C de saída.
 C ======================================================================
         program OrderedFile
-        integer i, i2, n,m, ios
+        integer i, i2, n,m, ios, swapped
         real line
         REAL*8, ALLOCATABLE :: numeros(:), menores(:), sorted_array(:)
         REAL*8 tmp
         
         open(unit=1, file='tarefa-3-entrada-1.in')
 
-        i = 0
+        i = 1
         do
             read (1, *, iostat=ios) line 
             if (ios /= 0) exit
@@ -27,11 +27,12 @@ C ======================================================================
 
         ALLOCATE(numeros(i))
         ALLOCATE(sorted_array(i))
-        REWIND(10)
+        REWIND(1)
 
         n = i        
-        do i2 = 1, i
-            read (1, iostat=ios) numeros(i2) 
+        do i2 = 1, i-1
+            read (1, *) numeros(i2)
+            ! print *, numeros(i2)
         end do
 
         close (1)
@@ -46,15 +47,22 @@ C ======================================================================
 
         ALLOCATE(menores(m))
 
-        do i = 1, n - 1
-            if (numeros(i) > numeros(i+1)) then
-                tmp = numeros(i)
-                numeros(i) = numeros(i+1)
-                numeros(i+1) = tmp
-            end if
+        swapped = 1;
+        do while (swapped >= 1)
+            swapped = 0;
+            do i = 1, n - 1
+
+                if (numeros(i) > numeros(i+1)) then
+                    tmp = numeros(i)
+                    numeros(i) = numeros(i+1)
+                    numeros(i+1) = tmp
+                    swapped = swapped + 1
+                    ! print *, "changening", numeros(i), numeros(i+1)
+                end if
+            end do
         end do
 
-        open(unit=2, file='tarefa-3-entrada-1.out')
+        open(unit=2, file='tarefa-3-entrada-1.out', action='write')
 
         do i = 1, m
             write (2, *) numeros(i)
